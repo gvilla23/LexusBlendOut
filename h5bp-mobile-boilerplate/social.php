92bccd07e489b6dc2fd7fe8f1bfa3a29bfff0414
George Villa <gvilla23@yahoo.com>
2013-01-28 17:56:21 -0800

    forgot one =(  , from previous upload 1-28-13

--- h5bp-mobile-boilerplate/social.php
+++ h5bp-mobile-boilerplate/social.php
@@ -4,26 +4,31 @@
   include_once("inc/header.php");
 ?>
 
- <!--start typing html/body text here -->
+<!-- page specific script -->
+ <script>
+$(document).ready(function(){
+$("#slideUpLinksContainer").trigger("click");
+ });
+</script>
+<!-- page specific script /-->
+
+<!--start typing html/body text here -->
  
 <div id="container" class="commonBG">
 <div id="shareText"></div>        
 
 	<ul id="socialIcons">
-		<li id="fb"><a href="http://www.facebook.com/sharer.php?s=100&amp;
-p[title]=The%20All%20New%20Lexus%20IS%20in%20Sports%20Illustrated&amp;
-p[url]=http%3A%2F%2Fwww.lexusblendout.com&amp;
-p[summary]=Check%20out%20the%20all-new%20Lexus%20IS%20in%20the%20Sports%20Illustrated%20Swimsuit%20Issue.%20It's%20the%20print%20ad%20that%20blends%20out.&amp;
-p[images][0]=http%3A%2F%2Fplacehold.it%2F100x56%2FFF0000"> &nbsp; </a></li>
-		
+	<li id="fb"><a href="https://www.facebook.com/sharer.php?s=100&amp;p[title]=The%20All%20New%20Lexus%20IS%20in%20Sports%20Illustrated&amp;p[url]=http%3A%2F%2Fwww.lexusblendout.com&amp;p[summary]=Check%20out%20the%20all-new%20Lexus%20IS%20in%20the%20Sports%20Illustrated%20Swimsuit%20Issue.%20It's%20the%20print%20ad%20that%20blends%20out.&amp;p[images][0]=http%3A%2F%2Fplacehold.it%2F100x56%2FFF0000">&nbsp;</a></li>
+
 		<li id="tweet"><a href="https://twitter.com/intent/tweet?
 text=Check%20out%20the%20all-new%20Lexus%20IS%20in%20the%20Sports%20Illustrated%20Swimsuit%20Issue.%20It's%20the%20print%20ad%20that%20blends%20out.&
 url=http%3A%2F%2Fwww.lexusblendout.com">&nbsp;</a></li>
 		
-		<li id="google"><a  href="#" class="blocklink"><br /></a></li>
+		<li id="google"><a  href="https://m.google.com/app/plus/x/?v=compose&content=[Check%20out%20the%20all-new%20Lexus%20IS%20in%20the%20Sports%20Illustrated%20Swimsuit%20Issue.%20It's%20the%20print%20ad%20that%20blends%20out.]%20[http%3A%2F%2Fwww.lexusblendout.com]">&nbsp;</a></li>
+
 		<li id="email"><a href="mailto:?
 subject=The%20All%20New%20Lexus%20IS%20in%20Sports%20Illustrated&
-body=Check%20out%20the%20all-new%20Lexus%20IS%20in%20the%20Sports%20Illustrated%20Swimsuit%20Issue.%20It's%20the%20print%20ad%20that%20blends%20out.%0A%0Ahttp%3A%2F%2Fwww.lexusblendout.com"> &nbsp; </a></li>
+body=Check%20out%20the%20all-new%20Lexus%20IS%20in%20the%20Sports%20Illustrated%20Swimsuit%20Issue.%20It's%20the%20print%20ad%20that%20blends%20out.%0A%0Ahttp%3A%2F%2Fwww.lexusblendout.com">&nbsp;</a></li>
 	</ul>
 
 <div id="copyLink"><input type="text" value="http://www.lexuxsblendout.com" size="20"></div>
